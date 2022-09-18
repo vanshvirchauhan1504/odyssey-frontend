047daaaad95d7fb25f44e82608e0472b270f0011
rockboy987 <62481809+rockboy987@users.noreply.github.com>
2022-09-18 11:41:44 +0530

    initial dashboard

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,71 +1,161 @@
+import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
+
 import React from 'react';
 import Select from 'react-select';
+import { Search, Option } from "searchpal";
 import makeAnimated from 'react-select/animated';
 import { useState } from 'react';
 import './Filter.css'
 const animatedComponents = makeAnimated();  
-  
+
+
+const genderTags = [
+    { label: "Male",value:11},
+    { label: "Female", value:12 },
+    { label: "Bigender", value: 13 }
+  ];
+
+const ageTags = [
+    { label: "Less than 18", value:11},
+    { label: "18-22", value:12 },
+    { label: "23-30", value: 13 }
+  ];
+
+const raceTags = [
+    { label: "Asian", value:11},
+    { label: "African American / Black", value:12 },
+  ];
+
+const countryTags = [
+    { label: "India", value:11},
+    { label: "USA", value:12 },
+]
+
+const getStarScore = value => {
+    if (value < 0.3)
+        return <div className="starRow"> <BsStar /> <BsStar /> <BsStar /> <BsStar /> <BsStar /> </div>
+    else if (value < 0.8)
+        return <div className="starRow"> <BsStarHalf /> <BsStar /> <BsStar /> <BsStar /> <BsStar /> </div>
+    else if (value < 1.2)
+        return <div className="starRow">  <BsStarFill /> <BsStar /> <BsStar /> <BsStar /> <BsStar /> </div>
+    else if (value < 1.8)
+        return <div className="starRow"> <BsStarFill /> <BsStarHalf /> <BsStar /> <BsStar /> <BsStar /> </div>
+    else if (value < 2.2)
+        return <div className="starRow"> <BsStarFill /> <BsStarFill /> <BsStar /> <BsStar /> <BsStar /> </div>
+    else if (value < 2.7)
+        return <div className="starRow"> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar /> <BsStar /> </div>
+    else if (value < 3.2)
+        return <div className="starRow"> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStar /> <BsStar /> </div>
+    else if (value < 3.7)
+        return <div className="starRow"> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar /> </div>
+    else if (value < 4.2)
+        return <div className="starRow"> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStar /> </div>
+    else if (value < 4.7)
+        return <div className="starRow"> <BsStarFill /><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> </div>
+    else 
+        return <div className="starRow"> <BsStarHalf /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> </div>
+}
+
 const Filter =() => { 
-    const [selectedOption, setSelectedOption] = useState();
-    const handleChange = (newOption) => {
-      setSelectedOption([...selectedOption,newOption]);
-      console.log(selectedOption);
-    }
-
-    const Gender = [
-        { label: "Male",value:11},
-        { label: "Female", value:12 },
-        { label: "Bigender", value: 13 }
-      ];
-
+    const [ genderTag, setGenderTag ] = useState();
+    const [ raceTag, setRaceTag ] = useState();
+    const [ countryTag, setCountryTag ] = useState();
+    const [ ageTag, setAgeTag ] = useState();
+    const [ selectedWorkplace, setSelectedWorkplace ] = useState();
+    const [ workplaceSuggestion, setWorkplaceSuggestions ] = useState([]);
+    const [ searchOpen, setSearchOpen ] = useState(false);
 
     return (
         <>
+        <h1>hello</h1>
         <div className="filter_container">
-            <div className="filter">
-                <Select options={Gender}
-                defaultValue={selectedOption} 
+            <div className="dropdowns">
+                <Select options={genderTags}
+                defaultValue={genderTag} 
                 components={animatedComponents}
-                value={selectedOption}
-                onChange= {setSelectedOption}
+                value={genderTag}
+                onChange= {setGenderTag}
                 />
 
-                <button onClick={() => console.log(selectedOption)}>test</button>
+                <Select options={raceTags}
+                defaultValue={raceTag} 
+                components={animatedComponents}
+                value={raceTag}
+                onChange= {setRaceTag}
+                />
+
+                <Select options={ageTags}
+                defaultValue={ageTag} 
+                components={animatedComponents}
+                value={ageTag}
+                onChange= {setAgeTag}
+                />
+
+                <Select options={countryTags}
+                defaultValue={countryTag} 
+                components={animatedComponents}
+                value={countryTag}
+                onChange= {setCountryTag}
+                />
+
+                <button onClick={() => console.log(genderTag, raceTag, countryTag, ageTag)}>test</button>
             </div>
-            {/* <div className="filter">
-                <Select options={Age} components={animatedComponents} isMulti />
+
+            <div className='workplaceSearchContainer'>
+                
+            <button onClick={() => setSearchOpen(true)}>Search for a workspace...</button>
+
+                <Search
+                    label="Search for a workspace..."
+                    open={searchOpen}
+                    onClose={() => setSearchOpen(false)}
+                    link={({ href, children }) => <a href={href}>{children}</a>}
+                >
+                    {/* {users.map((user) => (
+                    <Option
+                        label={user.name}
+                        sublabel={user.email}
+                        img={{ src: user.avatar, alt: `${user.name} profile picture` }}
+                        href={`/users/${user.id}`}
+                        keywords={(getKeywords) =>
+                        getKeywords(
+                            user.email,
+                            user.social && user.social.handle,
+                            user.organizations.map((org) => [
+                            org.name,
+                            org.locations.map((location) => [
+                                location.city,
+                                location.state,
+                            ]),
+                            ])
+                        )
+                        }
+                        key={user.id}
+                    >
+                        <Detail label="Joined" value={user.joined} />
+                        {user.organizations.length && (
+                        <Detail
+                            label="Organizations"
+                            value={<Organizations items={user.organizations} />}
+                        />
+                        )}
+                    </Option>
+                    ))} */}
+                </Search>
+
             </div>
-            <div className="filter">
-                <Select options={Place} components={animatedComponents} isMulti />
+
+            <div className='companyDetailsContainer'>
+                { /* Company questions - hide if not selected */ }
+                <div className="feedbackQuestionContainer">
+                    <text>abc</text>
+                    {getStarScore(2)}
+                </div>
+    
             </div>
-            <div className="filter">
-                <Select options={Race} components={animatedComponents} isMulti />
-            </div> */}
         </div>
         </>
     );
-  };
-
-
-   
-//   const Race = [
-//       { label: "Asian",value:21},
-//       { label: "African", value:22 },
-//       { label: "Caucasaian", value: 23 }
-//     ];
-  
-    // const Place = [
-    //   { label:"India",value:21},
-    //   { label: "USA", value:22 },
-    //   { label: "England", value: 23 }
-    // ];
-  
-//    const Age = [
-//       { label: "18-25",value:31},
-//       { label: "25-32", value:32 },
-//       { label: "32-40", value: 33 }
-//     ];
-    
- 
+  }; 
  
 export default Filter;
